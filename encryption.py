cdd1771597937a5277991172a833765c6a206d7b
PrayagD80 <spdasari20@gmail.com>
2025-02-24 22:30:31 +0530

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,25 @@
+import cv2
+import os
+
+img = cv2.imread("mypic.jpg")  # Replace with the correct image path
+
+msg = input("Enter secret message: ")
+password = input("Enter a passcode: ")
+
+d = {chr(i): i for i in range(255)}
+
+n, m, z = 0, 0, 0
+
+for char in msg:
+    img[n, m, z] = d[char]
+    n += 1
+    m += 1
+    z = (z + 1) % 3  # Cycle through RGB channels
+
+cv2.imwrite("encryptedImage.jpg", img)
+os.system("start encryptedImage.jpg")  # Open the image on Windows
+
+with open("password.txt", "w") as f:
+    f.write(password)  # Store password in a file
+
+print("Encryption complete. Encrypted image saved as 'encryptedImage.jpg'.")
